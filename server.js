ccd0f76c665016e32bfe05ce283e8d57909f25f4
codewithgabriel <olatoyeolaide@gmail.com>
2024-10-10 07:47:03 -0700

    update db

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const http = require('http');
 const { connect } = require('mongoose')
 connectDb().catch(err => console.log(err));
 
+
 async function connectDb() {
-  await connect('mongodb://127.0.0.1:27017/cloud-app');
+  await connect(process.env.DB_URL);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
