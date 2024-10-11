549cedaf26c222e0c57cadedc644ef67c96f7b77
codewithgabriel <olatoyeolaide@gmail.com>
2024-10-10 22:37:44 -0700

    socket addup

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ const PORT = 9000;
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/uploads', express.static('uploads')); 
 
 app.use(express.json());
